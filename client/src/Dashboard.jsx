ace4977a0727eaf0b7d5d284333c7b38be5a26d7
Chase Miller <a02283751@aggies.usu.edu>
2024-03-27 00:40:10 -0600

    reptile schedules

--- client/src/Dashboard.jsx
+++ client/src/Dashboard.jsx
@@ -1,24 +1,53 @@
 import { useNavigate } from "react-router-dom";
 import { requireLogin } from "./utils/require_login";
-import App from './App';
+import { useApi } from "./utils/use_api";
 import Schedule from './components/schedule';
+import { useEffect, useState } from "react";
 
 
 
 export const Dashboard = () => {
     const navigate = useNavigate();
-
-
+    const api = useApi();
     requireLogin();
 
-    function getReptiles() {}
+    const [reptiles, setReptiles] = useState([]);
+    const [user, setUser] = useState(null);
+
+    async function getUser() {
+        const {user} = await api.get("/users/me");
+        setUser(user);
+      }
+
+    async function getReptiles() {
+        const {reptiles} = await api.get(`/reptiles/user/${user.id}`);
+        setReptiles(reptiles);
+    }
+    
+    useEffect(() => {
+        getUser();
+    }, []);
+
+    useEffect(() => {
+        if (user) {
+            getReptiles();
+        }
+    }, [user]);
+
+
+    // useEffect(() => {
+    //     //when the reptiles change i should reload the data on the ui
+
+    // }, reptiles);
 
     return (
         <>
         <div>
             <h1>Dashboard</h1>
             <h2>Schedules</h2>
-            <Schedule></Schedule>
+            <Schedule
+            reptiles={reptiles}
+            />
             <h2>Reptiles</h2>
             <h2>Create Reptile</h2>
         </div>
--- client/src/components/Schedule.jsx
+++ client/src/components/Schedule.jsx
@@ -1,25 +1,44 @@
 import React from "react";
 export default Schedule;
 
-function Schedule() {
+function printTrueFalse(day) {
+    if (day) {
+        return "true";
+    }
+    else {
+        return "false";
+    }
+}
+
+function Schedule(props) {
     return (
         <>
             <table>
                 <tr>
                     <th>Reptile</th>
-                    <th>Contact</th>
-                    <th>Country</th>
+                    <th>Monday</th>
+                    <th>Tuesday</th>
+                    <th>Wednesday</th>
+                    <th>Thursday</th>
+                    <th>Friday</th>
+                    <th>Saturday</th>
+                    <th>Sunday</th>
                 </tr>
-                <tr>
-                    <td>Alfreds Futterkiste</td>
-                    <td>Maria Anders</td>
-                    <td>Germany</td>
-                </tr>
-                <tr>
-                    <td>Centro comercial Moctezuma</td>
-                    <td>Francisco Chang</td>
-                    <td>Mexico</td>
-                </tr>
+                
+                {props.reptiles.map((reptile) => {
+                    return (
+                    <tr key={reptile.id}>
+                        <td>{reptile.name}</td>
+                        <td>{printTrueFalse(reptile.monday)}</td>
+                        <td>{printTrueFalse(reptile.tuesday)}</td>
+                        <td>{printTrueFalse(reptile.wednesday)}</td>
+                        <td>{printTrueFalse(reptile.thursday)}</td>
+                        <td>{printTrueFalse(reptile.friday)}</td>
+                        <td>{printTrueFalse(reptile.saturday)}</td>
+                        <td>{printTrueFalse(reptile.sunday)}</td>
+                    </tr>
+                    )
+                })}
             </table>
         </>
     )
